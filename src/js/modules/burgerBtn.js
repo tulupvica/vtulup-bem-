7e72a339b493a923db6628a718056e2df31321af
Victoria Tulup <vtulup@justcoded.co>
2018-10-10 17:34:02 +0300

    add mein-header

---
+++ src/js/modules/burgerBtn.js
@@ -0,0 +1,26 @@
+const burgerBtn = {
+
+    burgerBtn: function () {
+
+      const $header = $('.main-header'),
+          $nav = $header.find('.main-nav'),
+          $navActive = ('main-nav_active'),
+          $btn = $header.find('.burger-btn'),
+          $btnActive = ('burger-btn_active'),
+          width = 768;
+
+        $btn.on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass($btnActive);
+            $nav.toggleClass($navActive);
+        });
+
+        $(window).on('resize', function () {
+            if($(window).width() > width) {
+                $nav.removeAttr('style');
+            }
+        });
+    }
+};
+
+export default burgerBtn;
