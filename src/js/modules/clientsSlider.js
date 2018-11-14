5735471dfb8d82825180d483cd57ff99b866fa2b
Victoria Tulup <vtulup@justcoded.co>
2018-11-14 13:49:23 +0200

    fix clients slider on rsize

--- src/js/modules/clientsSlider.js
+++ src/js/modules/clientsSlider.js
@@ -1,54 +1,64 @@
 const clientsSlider = {
 
-    clientsSlider: function () {
+  clientsSlider: function () {
 
-        const $slider = $('.clients-list');
-        const screenWidth = $(window).width();
-        const matchMedia = 768;
-
-
-        if(!$slider.length){
-            return;
-        }
-
-        function initSlider() {
-
-            $slider.slick({
-                infinite: true,
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                arrows: true,
-                prevArrow: '<button type="button" class="clients-list__prev-btn">' +
-                    '<img src="../../../assets/images/btn-prev.png" alt="prev">' +
-                    '</button>',
-                nextArrow: '<button type="button" class="clients-list__next-btn">' +
-                    '<img src="../../../assets/images/btn-next.png" alt="next">' +
-                    '</button>',
-                responsive: [
-                    {
-                        breakpoint: 481,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                ]
-            });
-        }
+    const $slider = $('.clients-list');
+    const matchMedia = 767;
 
 
-        if(screenWidth < matchMedia) {
-            initSlider();
-        }
-
-
-        $(window).on('resize', function(){
-            if(screenWidth > matchMedia) {
-                $slider.slick('unslick');
-            }
-        });
-
+    if (!$slider.length) {
+      return;
     }
+
+    function initSlider() {
+
+      $slider.slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: true,
+        prevArrow: '<button type="button" class="clients-list__prev-btn">' +
+            '<img src="../../../assets/images/btn-prev.png" alt="prev">' +
+            '</button>',
+        nextArrow: '<button type="button" class="clients-list__next-btn">' +
+            '<img src="../../../assets/images/btn-next.png" alt="next">' +
+            '</button>',
+        responsive: [
+          {
+            breakpoint: 481,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+        ]
+      });
+    }
+
+    function mobileSlider() {
+
+      $slider.each((item, slider) => {
+        /* Initializes a slick carousel only on mobile screens */
+        // slick on mobile
+
+        console.log(item, slider);
+        if ($(window).width() > matchMedia) {
+          if ($(slider).hasClass('slick-initialized')) {
+            $(slider).slick('unslick');
+          }
+        }
+        else {
+          if (!$(slider).hasClass('slick-initialized')) {
+            initSlider();
+          }
+        }
+      });
+    }
+
+
+    $(window).on('load resize', mobileSlider);
+
+  }
 };
 
 export default clientsSlider;
